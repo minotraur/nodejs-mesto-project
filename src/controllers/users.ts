6fe9eed8c2a1b0e9b9e7a4679e90da6ba0a05df1
Vlad <fedotov85050@gmail.com>
2024-07-15 22:18:04 +0300

    Добавлена обработка ошибок

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -16,10 +16,15 @@ export const getUsers = (req: Request, res: Response) => {
 
 export const getUsersById = (req: Request, res: Response) => {
   return User.findById(req.params.userId)
+    .orFail()
     .then((user) => res.send({ data: user }))
-    .catch(() =>
-      res.status(404).json({ message: "Запрашиваемый пользователь не найден" })
-    );
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        res.status(404).json({ message: "Запрашиваемый пользователь не найден" });
+      } else {
+        res.status(500).json({ message: "Ошибка сервера" });
+      }
+    });
 };
 
 export const createUser = async (req: Request, res: Response) => {
@@ -44,14 +49,23 @@ export const createUser = async (req: Request, res: Response) => {
 export const updateUser = async (req: Request, res: Response) => {
   const { name, about } = req.body;
   return User.findByIdAndUpdate(
-    "6693b4dd60c5309330a32aa2",
+    req.params.userId,
     { name, about },
     { new: true, runValidators: true }
   )
+    .orFail()
     .then((user) => res.send({ data: user }))
-    .catch((err) =>
-      res.status(404).json({ message: "Запрашиваемый пользователь не найден" })
-    );
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).json({
+          message: "Переданы некорректные данные при обновлении профиля",
+        });
+      } else if (err.name === "DocumentNotFoundError") {
+        res.status(404).json({ message: "Запрашиваемый пользователь не найден" });
+      } else {
+        res.status(500).json({ message: "Ошибка сервера" });
+      }
+    });
 };
 
 // Обновление аватара пользователя
@@ -59,12 +73,21 @@ export const updateAvatar = async (req: Request, res: Response) => {
   const { avatar } = req.body;
 
   return User.findByIdAndUpdate(
-    "6693b4dd60c5309330a32aa2",
+    req.params.userId,
     { avatar },
     { new: true, runValidators: true }
   )
+    .orFail()
     .then((user) => res.send({ data: user }))
-    .catch(() =>
-      res.status(404).json({ message: "Запрашиваемый пользователь не найден" })
-    );
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).json({
+          message: "Переданы некорректные данные при обновлении аватара",
+        });
+      } else if (err.name === "DocumentNotFoundError") {
+        res.status(404).json({ message: "Запрашиваемый пользователь не найден" });
+      } else {
+        res.status(500).json({ message: "Ошибка сервера" });
+      }
+    });
 };
